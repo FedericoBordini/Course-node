8dfbaa00ce746a40d6a64a1ce62c3e27157dbc0e
Federico Bordini <fedetrabajo13@gmail.com>
2023-07-31 19:34:03 -0300

    usando cmj y y cjs

---
+++ Primera-clase/cjs/sum.js
@@ -0,0 +1,5 @@
+function sum(a, b){
+    return a + b
+}
+//commonJS Module exports
+module.exports = sum
---
+++ Primera-clase/mjs/index.mjs
@@ -0,0 +1,8 @@
+import { sum, sub, mul } from './sum.mjs'
+
+console.log(sum(1, 2))
+console.log(sub(1, 2))
+console.log(mul(1, 2))
+
+
+
