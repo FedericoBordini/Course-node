b3f1710938658be19cb1d00afb98bb711b8338c4
Federico Bordini <fedetrabajo13@gmail.com>
2023-08-17 19:49:09 -0300

    Aumentando el OS

--- Primera-clase/1.os-info.js
+++ Primera-clase/1.os-info.js
@@ -10,3 +10,8 @@ console.log('CPUs', os.cpus()) // Esto ya es mas avanzado para escalar todo
 console.log('Memoria libre', os.freemem() / 1024 / 1024)
 console.log('Memoria total', os.totalmem() / 1024 / 1024)
 console.log('uptime', os.uptime() / 60 / 60)
+console.log('Directorio raiz', os.homedir())
+console.log('Nombre del host', os.hostname())
+console.log('Tipo de maquina', os.machine())
+console.log('Interfaces de internet', os.networkInterfaces())
+console.log('Devuelve el directorio temporal', os.tmpdir())
